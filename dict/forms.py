fe8018d7fd25f97c8569f53f515955eadb863992
artur.klimenko <artur.klimenko@dteam.dev>
2020-07-29 12:07:45 +0300

    form for create word was update, add some css.

--- dict/forms.py
+++ dict/forms.py
@@ -5,4 +5,11 @@ from dict.models import Word
 class CreateWordForm(forms.ModelForm):
     class Meta:
         model = Word
-        fields = '__all__'
+        fields = ['name', 'translate', 'transcription', 'phrase', 'sentence']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'translate': forms.TextInput(attrs={'class': 'form-control'}),
+            'transcription': forms.TextInput(attrs={'class': 'form-control'}),
+            'phrase': forms.TextInput(attrs={'class': 'form-control'}),
+            'sentence': forms.TextInput(attrs={'class': 'form-control'}),
+        }
